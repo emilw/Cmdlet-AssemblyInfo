8955b7b536e4717b5fa1f593eb31586b3022ae89
Emil <emil@postback.se>
2017-07-13 14:58:18 +0200

    Refactored e2e tests

--- Tests/e2e/Get-AssemblyInfoTests.ps1
+++ Tests/e2e/Get-AssemblyInfoTests.ps1
@@ -1,5 +1,15 @@
-﻿Write-Host "Starting e2e tests"
+﻿
+Write-Host "Starting e2e tests"
 $testFailed
+
+function ReportTestResult($testName, $expected, $actual){
+	if($expected -notmatch $actual){
+		$testFailed = 1
+		Write-Host -ForeGroundColor Red "Test: $testName failed. Expected: $expected vs. Actual: $actual"
+	} else{
+		Write-Host -ForeGroundColor Green "Test: $testName is ok"
+	}
+}
 
 $modulePath = Join-Path $PSScriptRoot ..\bin\Debug\Cmdlet-AssemblyInfo.dll
 Write-Host "Module path is $modulePath"
@@ -7,20 +17,16 @@ Write-Host "Module path is $modulePath"
 Import-Module $modulePath
 
 $testAssemblyPath = Join-Path $PSScriptRoot ..\bin\Debug\Tests.dll
+Write-Host "Test assembly path is $modulePath"
 
 Get-AssemblyTypes -AssemblyPath $testAssemblyPath
 
 $result = Get-AssemblyTypes -AssemblyPath $testAssemblyPath
+ReportTestResult "Empty filter parameter" 8 $result.length
 
-if($result.length -notmatch 8){
-    $testFailed = 1
-}
 
-$result = Get-AssemblyTypes -AssemblyPath $testAssemblyPath -Namespace "Test.Mock.DummyAttribute"
-
-if($result.length -notmatch 2){
-    $testFailed = 1
-}
+$result = Get-AssemblyTypes -AssemblyPath $testAssemblyPath -Attribute "Tests.Mock.DummyAttribute"
+ReportTestResult "Attribute filter test" 2 $result.length
 
 if($testFailed){
 	Write-Host -ForegroundColor Red "The e2e tests failed on one or more places"
