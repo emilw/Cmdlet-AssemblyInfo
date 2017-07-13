556dad693c55b1946840cc27312743d04f4c0d1a
Emil <emil@postback.se>
2017-07-13 14:38:08 +0200

    Added e2e tests

---
+++ Tests/e2e/Get-AssemblyInfoTests.ps1
@@ -0,0 +1,28 @@
+﻿Write-Host "Starting e2e tests"
+$testFailed
+
+Import-Module ..\bin\Debug\Cmdlet-AssemblyInfo.dll
+
+$testAssemblyPath = "..\bin\Debug\Tests.dll"
+
+Get-AssemblyTypes -AssemblyPath $testAssemblyPath
+
+$result = Get-AssemblyTypes -AssemblyPath $testAssemblyPath
+
+if($result.length -notmatch 8){
+    $testFailed = 1
+}
+
+$result = Get-AssemblyTypes -AssemblyPath $testAssemblyPath -Namespace "Test.Mock.DummyAttribute"
+
+if($result.length -notmatch 2){
+    $testFailed = 1
+}
+
+if($testFailed){
+	Write-Host -ForegroundColor Red "The e2e tests failed on one or more places"
+	return 1
+} else
+{
+	Write-Host -ForegroundColor Green "E2E tests was performed without errors"
+}
